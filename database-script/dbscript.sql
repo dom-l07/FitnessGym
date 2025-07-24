4820443d3ba5a6bc227922723d6b730307b22d77
Dom <dominiclim207@gmail.com>
2025-07-24 22:27:29 +0800

    Finalized Database Script

--- database-script/dbscript.sql
+++ database-script/dbscript.sql
@@ -11,7 +11,6 @@ CREATE TABLE `members` (
 	`dob` DATE NOT NULL,
 	`role` ENUM('user', 'admin') DEFAULT 'user',
     `gender` ENUM('Male', 'Female') NOT NULL,
-    `profile_picture` VARCHAR(255) DEFAULT 'defaultProfilePicture.jpg',
 PRIMARY KEY (`id`));
 
 -- Table `locations`
@@ -73,14 +72,18 @@ FOREIGN KEY (`member_id`) REFERENCES `members`(`id`));
 -- --------------------------------------------------------------------------------------
 -- Sample Data
 -- --------------------------------------------------------------------------------------
-INSERT INTO `members` (username, email, password, address, contact, dob, role, gender, profile_picture) VALUES
-("Lebron James", "james@gmail.com", "7c222fb2927d828af22f592134e8932480637c0d", "1 Woodlands Ave", 87654321, 2025-07-22, "admin", "Male", "defaultProfilePicture.jpg"),
-("Mary Tan", "mary@gmail.com", "7c222fb2927d828af22f592134e8932480637c0d", "Tampines Ave 1", 98765432, 2022-05-17, "user", "Female", "defaultProfilePicture.jpg");
+INSERT INTO `members` (username, email, password, address, contact, dob, role, gender) VALUES
+("Lebron James", "james@gmail.com", "7c222fb2927d828af22f592134e8932480637c0d", "1 Woodlands Ave", 87654321, 2002-07-22, "admin", "Male"),
+("Mary Tan", "mary@gmail.com", "7c222fb2927d828af22f592134e8932480637c0d", "Tampines Ave 1", 98765432, 2026-05-17, "user", "Female"),
+("John Lim", "john@gmail.com", "7c222fb2927d828af22f592134e8932480637c0d", "2 Sembawang Rd", 91234182, 2005-01-20, "user", "Male"),
+("Jamal Song", "jamal@gmail.com", "7c222fb2927d828af22f592134e8932480637c0d", "1 Novena Square", 95482378, 2000-04-18, "user", "Male");
 
 INSERT INTO `locations` (name, address) VALUES
 ("Tampines", "Tampines Ave 2"),
 ("Woodlands", "Woodlands Ave 1"),
-("Jurong East", "Jurong East St 21");
+("Jurong East", "Jurong East St 21"),
+("Orchard", "183 Orchard Road"),
+("City Hall", "1 Raffles Link");
 
 INSERT INTO `rooms` (location_id, room_name, capacity) VALUES
 (1, "Cardio", 30),
@@ -88,6 +91,9 @@ INSERT INTO `rooms` (location_id, room_name, capacity) VALUES
 (3, "Zumba", 40);
 
 INSERT INTO `classes` (room_id, location_id, class_name, class_type, instructor_name, class_start_time, class_end_time, max_participants) VALUES
-(),
-(),
-();
+(1, 1, "Run & Done", "Cardio", "Mrs Joanana", "2025-08-02 14:30:00", "2025-08-02 16:30:00", 30),
+(2, 2, "Lift to Live", "Strength", "Mr Joshua", "2025-08-07 17:00:00", "2025-08-07 18:30:00", 25),
+(3, 3, "Zumba Singa", "Zumba", "Mrs Lau", "2025-08-20 10:45:00", "2025-08-20 13:00:00", 40),
+(1, 1, "Zumba Roomba", "Zumba", "Mrs Tan", "2025-09-22 18:30:00", "2025-08-02 20:30:00", 30),
+(2, 2, "Feel the Burn", "Cardio", "Mr Thomas", "2025-08-17 15:00:00", "2025-08-07 16:30:00", 25),
+(3, 3, "Dominate the Game", "Strength", "Mr Toh", "2025-09-03 12:00:00", "2025-08-20 13:30:00", 15);
